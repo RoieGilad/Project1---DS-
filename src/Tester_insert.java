f2cefa8675f96df0516c62c79effadaa0591f2f1
TalNof <57536291+TalNof@users.noreply.github.com>
2021-11-22 16:28:51 +0200

    a

---
+++ src/Tester_insert.java
@@ -0,0 +1,10 @@
+
+
+
+public class Tester_insert {
+    public static void main(String[] args){
+        AVLTree tree = new AVLTree();
+        System.out.println(tree.getRoot().getParent());
+
+    }
+}
